375f5b99ac0b7bf922e9393c3efd815a264baf96
Ved108 <vedant.khanna@hotmail.com>
2026-04-03 14:00:33 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,21 @@
-// Function to show an alert when a button is clicked
-function sayHello() {
-    alert("Hello! You clicked the button!");
+function showLyrics(mantra) {
+    const lyricsDiv = document.getElementById('lyrics');
+
+    if(mantra === 'gayatri') {
+        lyricsDiv.innerHTML = `
+        Om Bhur Bhuvaḥ Swaḥ<br>
+        Tat-savitur Vareñyaṃ<br>
+        Bhargo Devasya Dhīmahi<br>
+        Dhiyo Yo Naḥ Prachodayāt
+        `;
+    } else if(mantra === 'mahamrityunjaya') {
+        lyricsDiv.innerHTML = `
+        Om Tryambakam Yajamahe<br>
+        Sugandhim Pushtivardhanam<br>
+        Urvarukamiva Bandhanan<br>
+        Mrityor Mukshiya Maamritat
+        `;
+    } else {
+        lyricsDiv.innerHTML = "Mantra not found!";
+    }
 }
